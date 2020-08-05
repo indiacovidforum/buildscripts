b9e81415cc775c66249f0da34adc9608cd33a7c0
Renzil DSouza <renzil@gmail.com>
2020-08-05 23:31:32 +0530

    Update build.sh

--- server/build.sh
+++ server/build.sh
@@ -1,11 +1,8 @@
 # install flarum from S3 (Flarum is built separately, as t2.micro instances cannot build some extensions needed)
 install-flarum.sh
 
-# setup elastic IP (needed for SSL to work)
-setup-elastic-ip.sh
-
 # generate ssl
-generate-ssl.sh
+install-ssl.sh
 
 # setup apache
 setup-apache.sh
