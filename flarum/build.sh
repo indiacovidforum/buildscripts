721d8b779e07d0003c6fecc3e4736a08a990384b
Renzil DSouza <renzil@gmail.com>
2020-08-06 14:38:07 +0530

    Update build.sh

--- flarum/build.sh
+++ flarum/build.sh
@@ -6,3 +6,17 @@ cd flarum
 
 # add google analytics extension (this requires more than 2GB of memory on the AWS instance)
 composer require fof/analytics
+
+# step out of flarum folder
+cd ..
+
+# zip the flarum build into an archive
+zip -r flarum.zip flarum
+
+# use this to manually copy the build at this point to local machine
+#scp -i flarum-build.pem bitnami@<EC2_PUBLIC_HOST_NAME>:~/flarum.zip .
+
+# tbd - get the latest flarum build number in S3
+# tbd - rename the flarum archive to <BUILD_NUMBER + 1>
+# tbd - upload flarum archive to S3
+# tbd - terminate instance
