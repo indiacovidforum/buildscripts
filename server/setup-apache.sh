3c5e4f5a19c6d651c18392f0d2ed325531601207
Renzil DSouza <renzil@gmail.com>
2020-08-06 15:06:46 +0530

    Update setup-apache.sh

--- server/setup-apache.sh
+++ server/setup-apache.sh
@@ -1,1 +1,5 @@
-# tbd
+# change server www root folder from '/opt/bitnamic/apache/htdocs' to '~/flarum/public'
+grep -rl '/opt/bitnami/apache/htdocs' /opt/bitnami/apache/conf/ | xargs sed -i 's|/opt/bitnami/apache/htdocs|~/flarum/public|g'
+
+# restart apache
+sudo /opt/bitnami/ctlscript.sh restart apache
