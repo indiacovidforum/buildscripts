2b21dd7b7baaf00bfb6bb7dbb0c09d33f8ffdbbd
Renzil DSouza <renzil@gmail.com>
2020-08-11 21:55:56 +0530

    Update launchserver.sh

--- launchserver.sh
+++ launchserver.sh
@@ -1,2 +1,9 @@
+ICF_AWS_PROFILE_NAME=launch
+ICF_AWS_IMAGE_ID=ami-0863ff60ae7cb7587
+ICF_AWS_INSTANCE_TYPE=t2.micro
+ICF_AWS_KEY_NAME=indiacovidforum-production
+ICF_AWS_SECURITY_GROUP_IDS=sg-0f5e87a31b186003c
+ICF_AWS_SUBNET_ID=subnet-0e6d6f66
+
 # launch AWS instance for indiacovidforum server 
-aws --profile launch ec2 run-instances --image-id ami-0863ff60ae7cb7587 --count 1 --instance-type t2.micro --key-name indiacovidforum-production --security-group-ids sg-0f5e87a31b186003c --subnet-id subnet-0e6d6f66
+aws --profile $ICF_AWS_PROFILE_NAME ec2 run-instances --image-id $ICF_AWS_IMAGE_ID --count 1 --instance-type $ICF_AWS_INSTANCE_TYPE --key-name $ICF_AWS_KEY_NAME --security-group-ids $ICF_AWS_SECURITY_GROUP_IDS --subnet-id $ICF_AWS_SUBNET_ID
