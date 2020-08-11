010499604e56bdefd75c07a90afd0f0548344d63
sharpshooter90 <sudipmelepunathilmp@gmail.com>
2020-08-12 01:06:26 +0530

    running the grep on ../apache/conf

--- server/setup-apache.sh
+++ server/setup-apache.sh
@@ -1,4 +1,4 @@
 # change server www root folder from '/opt/bitnamic/apache/htdocs' to '~/flarum/public'
-grep -rl '/opt/bitnami/apache/htdocs' /opt/bitnami/apache/conf/bitnami/ | xargs sudo sed -i  's|/opt/bitnami/apache/htdocs|/home/bitnami/flarum/public|g'
+grep -rl '/opt/bitnami/apache/htdocs' /opt/bitnami/apache/conf/ | xargs sudo sed -i  's|/opt/bitnami/apache/htdocs|/home/bitnami/flarum/public|g'
 # restart apache
 sudo /opt/bitnami/ctlscript.sh restart apache
