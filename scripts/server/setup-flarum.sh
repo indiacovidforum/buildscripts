1f9b904070e2c3448c93b01cfc5b1acf89ffc4ee
Renzil DSouza <renzil@gmail.com>
2020-08-12 21:36:00 +0530

    Updating setup-flarum scripts

--- scripts/server/setup-flarum.sh
+++ scripts/server/setup-flarum.sh
@@ -1,2 +1,6 @@
-# configure flarum app
+# update the public DNS hostname in the yml file
+publichostname=http://$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+sed -i 's/%PUBLIC_HOST_NAME%/$publichostname/g' devops/scripts/server/flarum-config.yml
+
+# configure flarum app from yml
 php /home/bitnami/flarum/flarum install --file=devops/scripts/server/flarum-config.yml
