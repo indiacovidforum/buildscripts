080dae0f641645af225a4594a9fd9282f8503e60
Renzil DSouza <renzil@gmail.com>
2020-08-12 21:40:48 +0530

    Fixing a bug with setup-flarum.sh

--- scripts/server/setup-flarum.sh
+++ scripts/server/setup-flarum.sh
@@ -1,6 +1,6 @@
 # update the public DNS hostname in the yml file
 publichostname=http://$(curl http://169.254.169.254/latest/meta-data/public-hostname)
-sed -i 's/%PUBLIC_HOST_NAME%/$publichostname/g' devops/scripts/server/flarum-config.yml
+sed -i "s/%PUBLIC_HOST_NAME%/$publichostname/g" devops/scripts/server/flarum-config.yml
 
 # configure flarum app from yml
 php /home/bitnami/flarum/flarum install --file=devops/scripts/server/flarum-config.yml
