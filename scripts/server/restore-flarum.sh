8121242c24f02d7082aacba7bccc0a2749677296
Renzil DSouza <renzil@gmail.com>
2020-08-12 20:47:08 +0530

    First cut script to restore flarum

--- scripts/server/restore-flarum.sh
+++ scripts/server/restore-flarum.sh
@@ -1,6 +1,28 @@
 # script to download and restore flarum
 
-# tbd - download flarum backup
-
 # mysql restore
-/opt/bitnami/mysql/bin/mysql -u root -pysbHfiirFBG0 flarumdb < flarum-sqlbkp_20200812_1597231357.bak
+restore_mysql() {
+  /opt/bitnami/mysql/bin/mysql -u root -p$(grep -o "'.*'" bitnami_credentials | sed "s/'//g") flarumdb < $1/mysql.bak
+}
+
+# flarum restore
+restore_flarum() {
+  cp -R $1/flarum/public/assets ~/flarum/public/assets
+}
+
+# download and restore flarum
+download_and_restore() {
+  backuptime=$1
+  backupdir=~/backups/$backuptime
+  backupfile=flarum-bkp_$backuptime.zip
+  mkdir -p $backuptime
+  aws s3 cp s3://indiacovidforum/backups/$backupfile $backupdir
+  cd $backupdir
+  unzip $backupdir/$backupfile
+  restore_mysql $backupdir &
+  restore_flarum $backupdir
+  wait
+  cd ~
+}
+
+download_and_restore $1
