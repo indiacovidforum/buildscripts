00625f3aa68e5fced113a5a48441686042883f5d
Renzil DSouza <renzil@gmail.com>
2020-08-12 19:22:33 +0530

    Fixing the backup-flarum script (not yet tested)

--- scripts/server/backup-flarum.sh
+++ scripts/server/backup-flarum.sh
@@ -1,8 +1,27 @@
-# script to backup and upload flarum
+# script to backup flarum and upload to S3
 
 # backup mysql
-/opt/bitnami/mysql/bin/mysqldump --lock-tables -u flarum -pw8wfBdjG4Yp3k-S% flarumdb > flarum-sqlbkp_`date +"%Y%m%d_%s"`.bak
+backup_mysql() {
+  /opt/bitnami/mysql/bin/mysqldump --lock-tables -u flarum -p$(grep -o "'.*'" bitnami_credentials | sed "s/'//g") flarumdb > $1/mysql.bak
+}
 
-# tbd - backup flarum
+# backup flarum
+backup_flarum() {
+  # add all flarum folders that need to be backed up here
+  cp -R ~/flarum/public/assets $1/flarum/public/assets
+}
 
-# tbd - upload backup
+# backup and upload
+backup_and_upload() {
+  backuptime=$(date +"%Y%m%d_%s")
+  backupdir=~/backups/$(backuptime)
+  backupfile=$(backupdir)/flarum-bkp_$(backuptime).zip
+  mkdir $(backupdir)
+  backup_mysql $(backupdir) &
+  backup_flarum $(backupdir)
+  wait
+  zip -r $(backupfile) $(backupdir)/*
+  aws s3 upload $(backupfile) s3://indiacovidforum/backups/
+}
+
+backup_and_upload
