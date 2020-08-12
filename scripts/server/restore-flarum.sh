87d55694b84b3ded18061017dadca1f10d051903
Renzil DSouza <renzil@gmail.com>
2020-08-12 21:56:40 +0530

    Forcing cp to overwrite without confirmation

--- scripts/server/restore-flarum.sh
+++ scripts/server/restore-flarum.sh
@@ -7,7 +7,7 @@ restore_mysql() {
 
 # flarum restore
 restore_flarum() {
-  cp -R $1/flarum/public/assets ~/flarum/public/
+  yes | cp -rf $1/flarum/public/assets ~/flarum/public/
 }
 
 # download and restore flarum
