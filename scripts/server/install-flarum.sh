e5198705e13a8cdc3728f01423cc1a4853594500
sharpshooter90 <sudipmelepunathilmp@gmail.com>
2020-08-14 01:16:01 +0530

    updated the unzip command for path fix

--- scripts/server/install-flarum.sh
+++ scripts/server/install-flarum.sh
@@ -2,7 +2,7 @@
 aws s3 cp s3://indiacovidforum/flarum-builds/1.zip /home/bitnami/flarum.zip
 
 # unzip flarum build
-unzip /home/bitnami/flarum.zip
+unzip /home/bitnami/flarum.zip -d /home/bitnami/
 
 # setting premissions
 sudo chown -R bitnami:daemon flarum
