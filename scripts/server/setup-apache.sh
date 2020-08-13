06930b7f84fbeec78f83c7724808da84742e8f3f
Renzil DSouza <renzil@gmail.com>
2020-08-13 20:27:41 +0530

    adding some whitspace

--- scripts/server/setup-apache.sh
+++ scripts/server/setup-apache.sh
@@ -1,4 +1,5 @@
 # change server www root folder from '/opt/bitnamic/apache/htdocs' to '~/flarum/public'
 grep -rl '/opt/bitnami/apache/htdocs' /opt/bitnami/apache/conf/ | xargs sudo sed -i  's|/opt/bitnami/apache/htdocs|/home/bitnami/flarum/public|g'
+
 # restart apache
 sudo /opt/bitnami/ctlscript.sh restart apache
