7f632393c5e3d101d4a6be7f6e89ce21aac51e18
Renzil DSouza <renzil@gmail.com>
2020-08-13 12:24:15 +0530

    Reverting previous commit

--- scripts/server/restore-flarum.sh
+++ scripts/server/restore-flarum.sh
@@ -7,7 +7,7 @@ restore_mysql() {
 
 # flarum restore
 restore_flarum() {
-  yes n | cp -rf $1/flarum/public/assets ~/flarum/public/
+  yes | cp -rf $1/flarum/public/assets ~/flarum/public/
 }
 
 # download and restore flarum
