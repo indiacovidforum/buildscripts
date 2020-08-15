e3050647d25e6b528511c42dce9a5e8e7b2b956c
Renzil DSouza <renzil@gmail.com>
2020-08-15 13:33:06 +0530

    Adding formatting extension to flarum build script

--- scripts/flarum/build.sh
+++ scripts/flarum/build.sh
@@ -4,6 +4,9 @@ composer create-project flarum/flarum flarum --stability=beta
 # switch to flarum installation folder
 cd flarum
 
+# add formatting extension
+composer require fof/formatting
+
 # add google analytics extension (this requires more than 2GB of memory on the AWS instance)
 composer require fof/analytics
 
